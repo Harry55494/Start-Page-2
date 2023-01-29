e229ff88f97ab4a1a57625b9ee659bfcaa6325c1
Harrison <harrison@phillingham.com>
2023-01-29 13:45:09 +0000

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<h1>Welcome to SvelteKit :)</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+
+
+<style>
+
+    :global(body){
+        background-color: #f0f0f0;
+        font-family: sans-serif;
+        text-align: center;
+        padding: 50px;
+    }
+
+</style>
